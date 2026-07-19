b00b10c7690f581f13c0b68829717fcdf1a08200
CoderDebasish <debasish.mohanty169@gmail.com>
2026-07-20 01:14:56 +0530

    feat: initialize PrintVerse project with Next.js, core dependencies, and root layout configuration

--- printverse/src/app/layout.tsx
+++ printverse/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -66,6 +67,7 @@ export default function RootLayout({
       <body>
         {children}
         <Analytics />
+        <SpeedInsights />
         <Toaster
           position="top-right"
           richColors
